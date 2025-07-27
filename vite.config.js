644618835bf3dafe55c11f7a6272a64202a5f359
Puffyjei <joel.cresmedi@gmail.com>
2025-07-27 21:05:38 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.', // <-- Asegura que apunta al directorio raíz
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html' 
+    }
   }
 })
